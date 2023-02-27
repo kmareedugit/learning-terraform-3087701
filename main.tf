47e059f027f46441d347540abdad27c28181b2e0
kmareedugit <126431031+kmareedugit@users.noreply.github.com>
2023-02-26 21:50:17 -0700

    Use Module for security group

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
@@ -34,6 +34,21 @@ resource "aws_security_group" "blog" {
   description = "Allow http & https in. Allow everything out"
   
   vpc_id = data.aws_vpc.default.id
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.1"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
