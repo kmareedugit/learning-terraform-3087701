0ca7ed11927247e38990d470bd833e8ae7f27924
kmareedugit <126431031+kmareedugit@users.noreply.github.com>
2023-02-26 19:46:46 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,8 +58,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
   
-  security_group_id = aws_security_group.blog.id
-  
+  security_group_id = aws_security_group.blog.id  
 }
 
 resource "aws_security_group_rule" "blog_everything_out {
@@ -70,5 +69,4 @@ resource "aws_security_group_rule" "blog_everything_out {
   cidr_blocks = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
-  
 }
